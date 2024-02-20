19ddc45499af0200a4239e578028437e8ee71417
Raivath Girish <raivathg@gmail.com>
2024-02-19 19:31:07 -0800

    three sample images and changed app.py

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+from fastai.vision.all import *
+
+def type_of_bear(x): return x[0].isupper() 
+categories = ('Black bear', 'Grizzly bear', 'Teddy Bear') 
+learn = load_learner('types_of_bear.pkl')
+def classify_image(img):
+    pred, idx, probs = learn.predict(img)
+    return dict(zip(categories, map(float,probs)))
+
+#|export
 import gradio as gr
-
-def greet(name):
-    return "Hello " + name + "!!"
-
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+# image = gr.inputs.Image(shape=(192, 192))
+# label = gr.outputs.Label()
+samples = ['grizzly_bear.jpg', 'teddy_bear.jpg', 'black_bear.jpg']
+intf = gr.Interface(fn=classify_image, inputs="image", outputs="label", examples=samples)
+intf.launch(inline=False)
